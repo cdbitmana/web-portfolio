c6cef93b9dc7f9693c5e08182e9b4c70e21aa6d4
cdbitmana <72531401+cdbitmana@users.noreply.github.com>
2021-06-14 11:45:12 +0900

    kakao map api

--- index.js
+++ index.js
@@ -625,3 +625,50 @@ var now = new Date();
 var year = now.getFullYear();
 document.getElementById('cr-year').innerText = year;
 /* copyright 올해연도 구하기 */
+
+/* kakao map api */
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+  center: new kakao.maps.LatLng(36.296223, 127.342619), //지도의 중심좌표.
+  level: 2, //지도의 레벨(확대, 축소 정도)
+  disableDoubleClick : true,
+  disableDoubleClickZoom : true
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+map.setZoomable(false);  
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(36.296223, 127.342619); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+window.addEventListener('resize', function () {
+  var center = new kakao.maps.LatLng(36.296223, 127.342619); //지도의 중심좌표.
+  map.setCenter(center);
+})
+
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomIn() {
+  map.setLevel(map.getLevel() - 1);
+}
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomOut() {
+  map.setLevel(map.getLevel() + 1);
+}
+
+function setCenter() {            
+  // 이동할 위도 경도 위치를 생성합니다 
+  var moveLatLon = new kakao.maps.LatLng(36.296223, 127.342619);
+  
+  // 지도 중심을 이동 시킵니다
+  map.panTo(moveLatLon);
+}
+/* kakao map api */
